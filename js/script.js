67e5ca7c9894ec959797e2a7426ec23c72a70771
Maramar520 <75872441+Maramar520@users.noreply.github.com>
2023-06-06 11:56:33 +0700

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+document.getElementById("kirim").addEventListener
+('click', function () {
+    var text = "Hello World!"
+    console.log(text)
+})
+
+document.getElementById("input-nama").addEventListener
+('input', function () {
+    var nama = document.getElementById("input-nama").value
+    console.log(nama)
+})
